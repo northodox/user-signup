97234fd8223ac7fdd63a07a803d8dd7cd569b6c6
Chris <northodontist@gmail.com>
2019-04-15 14:04:41 -0500

    made the index page actually show up

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from flask import Flask, request, redirect, render_template
 import cgi
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    error = request.args.get("error")
+    return render_template('index.html')
+
+app.run()
